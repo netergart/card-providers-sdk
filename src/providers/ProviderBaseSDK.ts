80116a3867a4438b0e77a663760bd7aec91e0653
Dmytro Shmalko <dima.shmalko@gmail.com>
2025-04-01 16:20:26 +0300

    feat: simplify error handling in ProviderBaseSDK by removing unnecessary console error logging

--- src/providers/ProviderBaseSDK.ts
+++ src/providers/ProviderBaseSDK.ts
@@ -55,10 +55,7 @@ export class ProviderBaseSDK {
     return axios
       .request(options)
       .then(({ data }) => data)
-      .catch((err) => {
-        // console.error("Error in request:", err);
-        return Promise.reject(this.getErrorMessage(err));
-      });
+      .catch((err) => Promise.reject(this.getErrorMessage(err)));
   };
 
   protected request = <T>(
