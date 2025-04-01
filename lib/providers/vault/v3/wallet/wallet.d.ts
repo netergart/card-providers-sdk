8dfa993c40bd4c5ec979be13cf7755da54f5f82e
Dmytro Shmalko <dima.shmalko@gmail.com>
2025-04-01 14:16:49 +0300

    feat: add account creation request and response interfaces to Wallet class

--- lib/providers/vault/v3/wallet/wallet.d.ts
+++ lib/providers/vault/v3/wallet/wallet.d.ts
@@ -1,7 +1,7 @@
 import { VaultV3SDKOptions } from "../../interfaces/vault.v3.interface";
 import { ErrorResponse } from "../interfaces/common.interface";
 import VaultProviderV3Base from "../VaultProviderBase";
-import { IWalletAccountInfoRequest, IWalletAccountInfoResponse, IWalletAddressListRequest, IWalletAddressRequest, IWalletAddressResponse, IWalletBalanceListResponse, IWalletGetBalanceLogListRequest, IWalletGetBalanceLogListResponse, IWalletGetBalanceLogReceiptRequest, IWalletGetBalanceLogReceiptResponse } from "./interfaces/wallet.interface";
+import { IWalletAccountCreateRequest, IWalletAccountCreateResponse, IWalletAccountInfoRequest, IWalletAccountInfoResponse, IWalletAddressListRequest, IWalletAddressRequest, IWalletAddressResponse, IWalletBalanceListResponse, IWalletGetBalanceLogListRequest, IWalletGetBalanceLogListResponse, IWalletGetBalanceLogReceiptRequest, IWalletGetBalanceLogReceiptResponse } from "./interfaces/wallet.interface";
 export declare class Wallet extends VaultProviderV3Base {
     constructor(options: VaultV3SDKOptions);
     accountInfo(body: IWalletAccountInfoRequest): Promise<IWalletAccountInfoResponse | ErrorResponse>;
@@ -11,4 +11,5 @@ export declare class Wallet extends VaultProviderV3Base {
     getBalanceLogList(body: IWalletGetBalanceLogListRequest): Promise<IWalletGetBalanceLogListResponse | ErrorResponse>;
     addressByAccount(body: IWalletAddressRequest): Promise<IWalletAddressResponse[] | ErrorResponse>;
     addressList(body: IWalletAddressListRequest): Promise<IWalletAddressResponse[] | ErrorResponse>;
+    createAccount(body: IWalletAccountCreateRequest): Promise<IWalletAccountCreateResponse | ErrorResponse>;
 }
