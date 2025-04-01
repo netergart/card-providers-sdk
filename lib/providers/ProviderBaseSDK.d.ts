cac02724e30d33dbe27b22c0e8adad8810f8add8
Dmytro Shmalko <dima.shmalko@gmail.com>
2025-04-01 16:00:24 +0300

    feat: implement detailed error handling in ProviderBaseSDK with getErrorMessage method

--- lib/providers/ProviderBaseSDK.d.ts
+++ lib/providers/ProviderBaseSDK.d.ts
@@ -6,6 +6,7 @@ export declare class ProviderBaseSDK {
     protected options: Required<ProviderBaseSDKOptions>;
     constructor(options?: Partial<ProviderBaseSDKOptions>);
     protected setOptions(options?: Partial<ProviderBaseSDKOptions>): void;
+    private getErrorMessage;
     private _request;
     protected request: <T>(path: string, options: AxiosRequestConfig) => Promise<T>;
 }
