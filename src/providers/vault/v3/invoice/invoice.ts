e9299248d344762868f4ff37312a53ffb7d314ce
Dmytro Shmalko <dima.shmalko@gmail.com>
2025-04-09 17:35:17 +0300

    fix: update confirm invoice path to remove versioning

--- src/providers/vault/v3/invoice/invoice.ts
+++ src/providers/vault/v3/invoice/invoice.ts
@@ -26,7 +26,7 @@ export class Invoice extends VaultProviderV3Base {
   }
 
   async confirm(body: ICreateInvoiceRequest): Promise<IInvoiceResponse> {
-    const path = "/acquiring/v1/invoice/payment/pay";
+    const path = "/acquiring/invoice/payment/pay";
 
     return await this.request(path, {
       method: "POST",
